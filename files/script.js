a9d5908edbff2695dd588c3082fc4b7a94bbf82f
SubliminalNexus <magicxturtle11@gmail.com>
2016-04-27 14:20:33 -0700

    Update script.js

--- files/script.js
+++ files/script.js
@@ -1,4 +1,11 @@
 var sel = $(".buttons select");
 function divHeight(ID, HEIGHT){
   $(ID).height(HEIGHT);
-}
+};
+function startup(){
+  $(".blockerino").hide();
+};
+startup();
+//Dev Functions, Remove from final version
+function block(ID){ $("ID").hide(); };
+function show(ID){  $("ID").show()  };
