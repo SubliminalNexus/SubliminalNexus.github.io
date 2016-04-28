7b05fccd6c1dc1bf5750f15db24a9a68d480219a
SubliminalNexus <magicxturtle11@gmail.com>
2016-04-28 14:13:56 -0700

    Update script.js

--- files/script.js
+++ files/script.js
@@ -2,10 +2,18 @@ var sel = $(".buttons select");
 function divHeight(ID, HEIGHT){
   $(ID).height(HEIGHT);
 };
+var quotes = 0;
+function quotecycle(Qs){
+  alert(quotes);
+  quotes++;
+}
+$("#submit").click(quotecycle(quotes));
+//On page startup
 function startup(){
   $(".blockerino").hide();
 };
 startup();
+
 //Dev Functions, Remove from final version
 function block(ID){ $(ID).hide() };
 function show(ID){  $(ID).show()  };
