0f90b8b4312df130c0875288caafb4c6d9077d81
SubliminalNexus <magicxturtle11@gmail.com>
2016-04-28 14:22:16 -0700

    Update script.js

--- files/script.js
+++ files/script.js
@@ -1,5 +1,4 @@
-$(function() {
-  var sel = $(".buttons select");
+var sel = $(".buttons select");
 function divHeight(ID, HEIGHT){
   $(ID).height(HEIGHT);
 };
@@ -7,7 +6,7 @@ var quotes = 0;
 function quotecycle(Qs){
   alert(quotes);
   quotes++;
-}
+};
 $("#submit").click(quotecycle(quotes));
 //On page startup
 function startup(){
@@ -18,4 +17,3 @@ startup();
 //Dev Functions, Remove from final version
 function block(ID){ $(ID).hide() };
 function show(ID){  $(ID).show()  };
-});
