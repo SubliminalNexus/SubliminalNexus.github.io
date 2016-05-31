752833b70ac897f6c19015869ce5d2b79f2812a0
SubliminalNexus <magicxturtle11@gmail.com>
2016-05-31 03:32:26 -0700

    Update script.js

--- files/script.js
+++ files/script.js
@@ -1,3 +1,13 @@
+var quotes = [
+  "There is no one right way to live.",
+  "There's nothing fundamentally wrong with people.",
+  "Perhaps the flaw in man is that he doesn't know how he ought to live.",
+  "Man is the end product of creation.",
+  "Man is the creature for whom all the rest was made: this world, this solar system, this galaxy, the universe itself.",
+  "Perhaps the flaw in man is exactly this: that he doesn't know how he ought to live.",
+  "The species are not in any sense at war with one another.",
+  "The laws they make in Washington aren't put on the books because they work well, they're put on the books because they represent the one right way to live.",
+  ]
 var sel = $(".buttons select");
 function divHeight(ID, HEIGHT){
   $(ID).height(HEIGHT);
